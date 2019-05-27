f4219feaf0b9ba5e25f4374b84768bbea89528b3
rory513 <m.akbar.iskandar@gmail.com>
2019-05-27 08:06:10 +0700

    add print to csv command

--- main.py
+++ main.py
@@ -9,10 +9,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-m', '--message', type=str)
 parser.add_argument('-t', '--test', action='store_true')
 parser.add_argument('-l', '--length', type=int, default=100)
+parser.add_argument('-p', '--print', action='store_true')
 
 args = parser.parse_args()
 
-if args.message:
+if args.print:
+    classifier.trainer.save_words_to_csv('word_features.csv')
+    classifier.trainer.save_preprocessed_data()
+elif args.message:
     classifier.classify_message(args.message)
 elif args.test:
     classifier.print_prediction(args.length)
